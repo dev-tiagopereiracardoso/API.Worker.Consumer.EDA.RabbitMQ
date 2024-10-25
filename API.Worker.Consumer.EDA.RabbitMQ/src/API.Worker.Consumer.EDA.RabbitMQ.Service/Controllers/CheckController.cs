55e886835d554bc6c07350cd55ed1a8d1d8e6e4c
Tiago Pereira Cardoso <tiagocardosso@gmail.com>
2024-10-25 10:37:43 +0200

    Create API for active Consumer if inactive queue

---
+++ API.Worker.Consumer.EDA.RabbitMQ/src/API.Worker.Consumer.EDA.RabbitMQ.Service/Controllers/CheckController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Worker.Consumer.EDA.RabbitMQ.Service.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("check")]
+    public class CheckController : ControllerBase
+    {
+        public CheckController()
+        {
+        }
+
+        [HttpGet("system")]
+        public bool System()
+        {
+            return true;
+        }
+    }
+}
